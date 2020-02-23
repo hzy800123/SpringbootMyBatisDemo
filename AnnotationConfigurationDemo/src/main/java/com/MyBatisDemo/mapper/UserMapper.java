5b170e58af1e9da26ea3bbc5f21f1d11d014f623
John <hzy800@126.com>
2020-02-23 17:39:29 +0800

    Introduce resultMap to the Result mapping of Interface

--- AnnotationConfigurationDemo/src/main/java/com/MyBatisDemo/mapper/UserMapper.java
+++ AnnotationConfigurationDemo/src/main/java/com/MyBatisDemo/mapper/UserMapper.java
@@ -17,14 +17,14 @@ public interface UserMapper {
     @Select("select id, user_name, pass_word, age, reg_time from user where id = #{id}")
     User selectUser(int id);
 
-
-    @Results({
-            @Result(id = true, property = "id", column = "id"),
-            @Result(property = "userName", column = "user_name"),
-            @Result(property = "passWord", column = "pass_word"),
-            @Result(property = "age", column = "age"),
-            @Result(property = "regTime", column = "reg_time")
-    })
+//    @Results({
+//            @Result(id = true, property = "id", column = "id"),
+//            @Result(property = "userName", column = "user_name"),
+//            @Result(property = "passWord", column = "pass_word"),
+//            @Result(property = "age", column = "age"),
+//            @Result(property = "regTime", column = "reg_time")
+//    })
+    @ResultMap("mybatis.mappers.UserMapper.UserResult")
     @Select("select * from user")
     List<User> retrieveAllUser();
 
