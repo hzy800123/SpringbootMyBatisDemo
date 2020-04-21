38b701180d9a98bef3d91bd034c66f7e024e6fb1
John <hzy800@126.com>
2020-04-21 21:52:55 +0800

    在 启动类 解决 跨域问题

--- RestfulWebService-Springboot/src/main/java/com/testSpringBootWeb/demo/DemoApplication.java
+++ RestfulWebService-Springboot/src/main/java/com/testSpringBootWeb/demo/DemoApplication.java
@@ -2,9 +2,21 @@ package com.testSpringBootWeb.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements WebMvcConfigurer {
+
+	// 启动类 实现 接口WebMvcConfigurer，重写 addCorsMappings方法，即可实现CORS 解决跨域问题。
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowCredentials(true)
+				.allowedHeaders("*")
+				.allowedOrigins("*")
+				.allowedMethods("*");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
